74b75badf69372e35efb0e8d2bc0ba1a0c9a4497
Chris Connor <cjconnor24@connordesign.com>
2018-05-10 13:55:47 +0100

    Adding functions and event listeners into the controller

--- app.js
+++ app.js
@@ -16,6 +16,31 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var ctrlAddItem = function(){
 
+          
+        // GET VALUES
+
+        // ADD ITEM TO BUDGET CONTROLLER
+
+        // ADD TO UI CONTROLLER
+
+        // CALCUL THE BUDGET
+
+        // DISPLAY THE BUDGET ON THE UI
+        console.log("It works.");
+
+    };
+
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+
+    document.addEventListener('keypress',function(e){       
+
+        if(e.keyCode === 13 || event.which === 13){
+            console.log("Enter was pressed 👍");
+            ctrlAddItem();            
+        }
+
+    })
 
 })(budgetController, UIController);
