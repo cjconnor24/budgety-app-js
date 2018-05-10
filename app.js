dd399adc553c602cbffca4394ba6fa607032d19e
Chris Connor <cjconnor24@connordesign.com>
2018-05-10 14:06:13 +0100

    Added dom object to hold all dom string elements. Will be easier for re-use

--- app.js
+++ app.js
@@ -9,17 +9,43 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function(){
 
+    // DOMstrings to reduce typing
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+
     // SOME CODE
+    return {
+        getInput: function(){
+            
+            return {
+            type: document.querySelector(DOMstrings.inputType).value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            }
+            
+        },
+        getDOMStrings: function(){
+            return DOMstrings;
+        }
+    };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function(){
 
           
         // GET VALUES
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // ADD ITEM TO BUDGET CONTROLLER
 
@@ -28,16 +54,15 @@ var controller = (function(budgetCtrl, UICtrl){
         // CALCUL THE BUDGET
 
         // DISPLAY THE BUDGET ON THE UI
-        console.log("It works.");
+        // console.log("It works.");
 
     };
 
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
 
     document.addEventListener('keypress',function(e){       
 
         if(e.keyCode === 13 || event.which === 13){
-            console.log("Enter was pressed 👍");
             ctrlAddItem();            
         }
 
